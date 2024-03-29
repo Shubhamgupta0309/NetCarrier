3c5790a183ffffec949e3728904070715d341043
Shubham Gupta <122101804+Shubhamgupta0309@users.noreply.github.com>
2024-03-29 14:28:20 +0530

    Delete server/services directory

--- server/services/userService.js
+++
@@ -1,8 +0,0 @@
-// services/userService.js
-const User = require('../models/User');
-
-const getUserById = async (userId) => {
-  // Function to get user by ID from the database
-};
-
-module.exports = { getUserById };
